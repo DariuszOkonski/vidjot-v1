5a909ab2b8047eca7370ea1ada052a1f57709195
Dariusz Okoński <darek200180@gmail.com>
2022-10-26 12:48:47 +0200

    add idea to db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -12,10 +13,14 @@ mongoose
 require("./models/Idea");
 const Idea = mongoose.model("ideas");
 
-//View engine setup
+// middleware
 app.set("view engine", "handlebars");
 app.engine("handlebars", exphbs.engine());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
 app.get("/", (req, res) => {
   const title = "Welcome";
   return res.render("index", {
@@ -25,6 +30,41 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   return res.render("about");
+});
+
+app.get("/ideas/add", (req, res) => {
+  return res.render("ideas/add");
+});
+
+app.post("/ideas", (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please add some details" });
+  }
+
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors,
+      title: req.body.title,
+      details: req.body.details,
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details,
+    };
+
+    new Idea(newUser).save().then((idea) => {
+      res.redirect("/ideas");
+    });
+  }
+});
+
+app.get("/ideas", (req, res) => {
+  return res.send("Welcome ideas");
 });
 
 const port = 5000;
