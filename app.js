aafe0deba57efd3feadbc9a686901a7092e65613
Dariusz Okoński <darek200180@gmail.com>
2022-10-26 14:07:24 +0200

    add delete idea route

--- app.js
+++ app.js
@@ -106,6 +106,14 @@ app.put("/ideas/:id", (req, res) => {
   });
 });
 
+app.delete("/ideas/:id", (req, res) => {
+  Idea.findByIdAndDelete({
+    _id: req.params.id,
+  }).then(() => {
+    res.redirect("/ideas");
+  });
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
